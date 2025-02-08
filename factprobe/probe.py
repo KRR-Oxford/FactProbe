89dbd77bb25d82bcdd1fc84710d749e8e5b021fa
Lawhy <yuanhe.cs.ai@gmail.com>
2025-02-08 00:15:15 +0000

    [feat] probe

---
+++ factprobe/probe.py
@@ -0,0 +1,95 @@
+# Copyright 2025 Yuan He
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pandas as pd
+import itertools
+import random
+from collections import defaultdict
+from tqdm.auto import tqdm
+from vllm import LLM, SamplingParams
+from factprobe.prompt import QuestionAnsweringPrompt, FactCheckingPrompt
+
+
+class FactProbe:
+    def __init__(
+        self,
+        llm: LLM,
+        template_type: str,
+        template_forward: str,
+        template_backward: str,
+        relation_forward: str,
+        relation_backward: str,
+        **kwargs,
+    ):
+        self.llm = llm
+        self.template_type = template_type
+        assert self.template_type in ["qa", "fc"], (
+            f"Invalid template type: {template_type}"
+        )
+        self.prompt_cls = {"qa": QuestionAnsweringPrompt, "fc": FactCheckingPrompt}[
+            template_type
+        ]
+        self.prompt_forward = self.prompt_cls(template=template_forward)
+        self.prompt_backward = self.prompt_cls(template=template_backward)
+        self.relation_forward = relation_forward
+        self.relation_backward = relation_backward
+        self.correct = {"qa": "yes", "fc": "true"}[self.template_type]
+
+    def probe(self, data: pd.DataFrame, so_setting: str):
+        print(f"[{self.template_type}] probing for [{so_setting}]:")
+        inputs_forward = []
+        inputs_backward = []
+        keys = []
+        # collect and format inputs
+        for _, dp in tqdm(
+            data.iterrows(), total=len(data), desc="Preprocessed triples"
+        ):
+            for s, o in itertools.product(
+                eval(dp["subject_name"]), eval(dp["object_name"])
+            ):
+                keys.append((dp["subject"], dp["object"]))
+                inputs_forward.append(
+                    self.prompt_forward.render((s, self.relation_forward, o))
+                )
+                inputs_backward.append(
+                    self.prompt_backward.render((s, self.relation_backward, o))
+                )
+        print("Example forward inputs:\n", random.choice(inputs_forward))
+        print("Example backward inputs:\n", random.choice(inputs_backward))
+
+        # compute forward outputs
+        outputs_forward = self.llm.chat(
+            inputs_forward, SamplingParams(logprobs=10, top_p=0.95)
+        )
+        results_forward = defaultdict(list)
+        for output, k in zip(outputs_forward, keys):
+            results_forward[k].append(output.outputs[0].text.lower() == self.correct)
+        count_forward = 0
+        for _, v in results_forward.items():
+            count_forward += int(any(v))
+
+        # compute backwardward outputs
+        outputs_backward = self.llm.chat(
+            inputs_backward, SamplingParams(logprobs=10, top_p=0.95)
+        )
+        results_backward = defaultdict(list)
+        for output, k in zip(outputs_backward, keys):
+            results_backward[k].append(output.outputs[0].text.lower() == self.correct)
+        count_backward = 0
+        for _, v in results_backward.items():
+            count_backward += int(any(v))
+
+        print(f"{count_forward}-{count_backward} / {len(data)} ({len(inputs_forward)})")
+
+        return {"forward": results_forward, "backward": results_backward}
--- probe.py
+++ probe.py
@@ -1,12 +1,23 @@
+# Copyright 2025 Yuan He
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import click
 import pandas as pd
-import itertools
-from tqdm.auto import tqdm
-from vllm import LLM, SamplingParams
-from factprobe.prompt import QuestionAnsweringPrompt, FactCheckingPrompt
-from collections import defaultdict
+from vllm import LLM
+from factprobe.probe import FactProbe
 from deeponto.utils import save_file, load_file
 from yacs.config import CfgNode
-import click
 
 
 @click.command()
@@ -20,7 +31,6 @@ def main(config_file: str):
     df = pd.read_csv(f"data/cleaned/{relation}_triples.csv")
 
     # 2. Filter the data according to the setting
-    setting = config.setting
     data = {
         "high2low": df[
             (df["subject_count"] >= config.count_high)
@@ -30,72 +40,21 @@ def main(config_file: str):
             (df["subject_count"] <= config.count_low)
             & (df["object_count"] >= config.count_high)
         ],
-    }[setting]
+    }
 
     # 3. Initialise the LLM model
     llm = LLM(model=config.model, dtype="half")
 
-    # 4. Set up the prompt
-    template_type = config.template_type
-    prompt_cls = {"qa": QuestionAnsweringPrompt, "fc": FactCheckingPrompt}[
-        template_type
-    ]
-    forward_prompt = prompt_cls(
-        template=getattr(config, f"{template_type}_template_forward")
-    )
-    backward_prompt = prompt_cls(
-        template=getattr(config, f"{template_type}_template_backward")
-    )
-    
-    # 5. Probing the model
-    print(f"[{template_type}] probing for [{setting}]:")
-    inputs_forward = []
-    inputs_backward = []
-    keys = []
-    for _, dp in tqdm(data.iterrows()):
-        for s, o in itertools.product(
-            eval(dp["subject_name"]), eval(dp["object_name"])
-        ):
-            keys.append((dp["subject"], dp["object"]))
-            inputs_forward.append(
-                forward_prompt.render(
-                    (s, getattr(config, f"{template_type}_relation_forward"), o)
-                )
-            )
-            inputs_backward.append(
-                backward_prompt.render(
-                    (s, getattr(config, f"{template_type}_relation_backward"), o)
-                )
-            )
-    print("Example forward inputs:\n", inputs_forward[0])
-    print("Example backward inputs:", inputs_backward[0])
+    # 4. Probe the model
+    probe = FactProbe(llm=llm, **config)
 
-    outputs_forward = llm.chat(
-        inputs_forward, SamplingParams(logprobs=10, top_p=0.95)
-    )
-    result_dict_forward = defaultdict(list)
-    for output, k in zip(outputs_forward, keys):
-        result_dict_forward[k].append(output.outputs[0].text.lower() == "yes")
-    count_forward = 0
-    for _, v in result_dict_forward.items():
-        count_forward += int(any(v))
-
-    outputs_backward = llm.chat(
-        inputs_backward, SamplingParams(logprobs=10, top_p=0.95)
-    )
-    result_dict_backward = defaultdict(list)
-    for output, k in zip(outputs_backward, keys):
-        result_dict_backward[k].append(output.outputs[0].text.lower() == "yes")
-    count_backward = 0
-    for _, v in result_dict_backward.items():
-        count_backward += int(any(v))
-
-    print(f"{count_forward}-{count_backward} / {len(data)} ({len(inputs_forward)})")
-
-    save_file(
-        {"forward": result_dict_forward, "backward": result_dict_backward},
-        f"experiments/{relation}/{relation}_{config.count_high}_{config.count_low}_{setting}_{template_type}.pkl",
-    )
+    # 5. Save the results
+    for so_setting in data.keys():
+        results = probe.probe(data[so_setting], so_setting)
+        save_file(
+            {"forward": results["forward"], "backward": results["backward"]},
+            f"experiments/{config.relation}/{config.relation}_{config.count_high}_{config.count_low}_{so_setting}_{config.template_type}.pkl",
+        )
 
 
 if __name__ == "__main__":
